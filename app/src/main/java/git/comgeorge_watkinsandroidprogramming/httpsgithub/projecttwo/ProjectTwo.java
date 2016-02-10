a0abafe7914e4d4eb182bc0e5551278f98ae8a6a
George-Watkins <george.watkins.13@cnu.edu>
2016-02-10 13:06:09 -0500

    Initial commit

---
+++ app/src/main/java/git/comgeorge_watkinsandroidprogramming/httpsgithub/projecttwo/ProjectTwo.java
@@ -0,0 +1,13 @@
+package git.comgeorge_watkinsandroidprogramming.httpsgithub.projecttwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_two);
+    }
+}
