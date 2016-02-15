cce1ea74d1ba3ddbc954c12d86738278e0d5550e
George-Watkins <george.watkins.13@cnu.edu>
2016-02-15 16:29:32 -0500

    Changed the layout.  Made the background visible and made the camera button available

--- app/src/main/java/git/comgeorge_watkinsandroidprogramming/httpsgithub/projecttwo/ProjectTwo.java
+++ app/src/main/java/git/comgeorge_watkinsandroidprogramming/httpsgithub/projecttwo/ProjectTwo.java
@@ -2,6 +2,7 @@ package git.comgeorge_watkinsandroidprogramming.httpsgithub.projecttwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProjectTwo extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ProjectTwo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_two);
+    }
+
+
+    public void goCamera(View view) {
     }
 }
